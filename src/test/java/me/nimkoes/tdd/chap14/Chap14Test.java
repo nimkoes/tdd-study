59e01fc54b9c82835176cef0b72c6ac61c46dbcd
nimkoes <xxxelppa@gmail.com>
2023-07-12 13:20:33 +0900

    14장 바꾸기 - 필요할 거라고 생각한 인자를 빠르게 추가했다. - 코드와 테스트 사이에 있는 데이터 중복을 끄집어냈다. - 자바의 오퍼레이션에 대한 가정을 검사해보기 위한 테스트 (testArrayEquals) 를 작성했다. - 별도의 테스트 없이 전용 (private) 도우미 (helper) 클래스를 만들었다, - 리팩토링하다가 실수를 했고, 그 문제를 분리하기 위해 또 하나의 테스트를 작성하면서 계속 전진해 가기로 선택했다.

--- src/test/java/me/nimkoes/tdd/chap14/Chap14Test.java
+++ src/test/java/me/nimkoes/tdd/chap14/Chap14Test.java
@@ -1,3 +1,4 @@
+
 package me.nimkoes.tdd.chap14;
 
 import org.junit.jupiter.api.Test;
@@ -60,5 +61,19 @@ public class Chap14Test {
         Money result = bank.reduce(Money.dollar(1), "USD");
 
         assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
     }
 }
