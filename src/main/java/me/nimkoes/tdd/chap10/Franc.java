9c831f85191c2583c0272a0c4da0f1ff3d22f62b
nimkoes <xxxelppa@gmail.com>
2023-07-12 13:13:11 +0900

    10장 흥미로운 시간 - 두 times() 를 일치시키기 위해 그 메소드들이 호출하는 다른 메소드들을 인라인시킨 후 상수를 변수로 바꿔주었다. - 단지 디버깅을 위해 테스트 없이 toString() 을 작성했다. - Franc 대신 Money 를 반환하는 변경을 시도한 뒤 그것이 잘 작동할지를 테스트가 말하도록 했다. - 실험해본 걸 뒤로 물리고 또 다른 테스트를 작성했다. 테스트를 작동했더니 실험도 제대로 작동했다.

--- src/main/java/me/nimkoes/tdd/chap10/Franc.java
+++ src/main/java/me/nimkoes/tdd/chap10/Franc.java
@@ -7,6 +7,6 @@ public class Franc extends Money {
     }
 
     Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
+        return new Money(amount * multiplier, currency);
     }
 }
--- src/main/java/me/nimkoes/tdd/chap10/Money.java
+++ src/main/java/me/nimkoes/tdd/chap10/Money.java
@@ -1,6 +1,6 @@
 package me.nimkoes.tdd.chap10;
 
-public abstract class Money {
+public class Money {
 
     protected int amount;
     protected String currency;
@@ -10,7 +10,9 @@ public abstract class Money {
         this.currency = currency;
     }
 
-    abstract Money times(int multiplier);
+    Money times(int amount){
+        return null;
+    }
 
     String currency() {
         return currency;
@@ -28,6 +30,11 @@ public abstract class Money {
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 }
