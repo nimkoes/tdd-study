eaae49c7393595cfd77c04d9214dc97b144a91a6
nimkoes <xxxelppa@gmail.com>
2023-07-12 12:54:47 +0900

    8장 객체 만들기

---
+++ src/main/java/me/nimkoes/tdd/chap08/Money.java
@@ -0,0 +1,12 @@
+package me.nimkoes.tdd.chap08;
+
+public class Money {
+    protected int amount;
+
+    @Override
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && getClass().equals(money.getClass());
+    }
+}
