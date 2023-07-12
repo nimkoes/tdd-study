6de52da0099f515cbbc9304bbf50cf1455862eff
nimkoes <xxxelppa@gmail.com>
2023-07-12 12:43:24 +0900

    3장 모두를 위한 평등

---
+++ src/main/java/me/nimkoes/tdd/chap03/Dollar.java
@@ -0,0 +1,13 @@
+package me.nimkoes.tdd.chap03;
+
+public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
---
+++ src/test/java/me/nimkoes/tdd/chap03/Chap03Test.java
@@ -0,0 +1,17 @@
+package me.nimkoes.tdd.chap03;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Chap03Test {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        Assertions.assertEquals(10, product.amount);
+        product = five.times(3);
+        Assertions.assertEquals(15, product.amount);
+    }
+}
