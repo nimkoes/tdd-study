c4b6ba0544c5585e584c15074a3c0938ea40ab7f
nimkoes <xxxelppa@gmail.com>
2023-07-12 12:45:17 +0900

    4장 프라이버시

---
+++ src/main/java/me/nimkoes/tdd/chap04/Dollar.java
@@ -0,0 +1,19 @@
+package me.nimkoes.tdd.chap04;
+
+public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
+}
---
+++ src/test/java/me/nimkoes/tdd/chap04/Chap04Test.java
@@ -0,0 +1,23 @@
+package me.nimkoes.tdd.chap04;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Chap04Test {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        Assertions.assertEquals(10, product.amount);
+        product = five.times(3);
+        Assertions.assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        Assertions.assertTrue(new Dollar(5).equals(new Dollar(5)));
+        Assertions.assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+}
