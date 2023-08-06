4c1c8eda4dd556f2687d5faf5e66cae8d43a9d87
nimkoes <xxxelppa@gmail.com>
2023-08-06 19:26:00 +0900

    부록 B. 피보나치 - 테스트 케이스를 추가하려면 키보드를 여섯 번만 치면 된다. - 공교롭게도 테스트가 통과한다.

--- src/test/java/me/nimkoes/tdd/appendixB/Fibonacci.java
+++ src/test/java/me/nimkoes/tdd/appendixB/Fibonacci.java
@@ -8,7 +8,7 @@ public class Fibonacci {
 
     @Test
     public void testFibonacci() {
-        int cases[][] = {{0, 0}, {1, 1}};
+        int cases[][] = {{0, 0}, {1, 1}, {2, 1}};
         for (int i = 0; i < cases.length; i++) {
             assertEquals(cases[i][1], fib(cases[i][0]));
         }
