017333e1408b1f051b2aa059fee4e65fb6771752
nimkoes <xxxelppa@gmail.com>
2023-08-06 19:19:38 +0900

    부록 B. 피보나치 - fib(0) 의 결과는 0 이다. - 구현은 상수를 반환한다.

--- src/test/java/me/nimkoes/tdd/appendixB/Fibonacci.java
+++ src/test/java/me/nimkoes/tdd/appendixB/Fibonacci.java
@@ -1,4 +1,16 @@
 package me.nimkoes.tdd.appendixB;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class Fibonacci {
+
+    @Test
+    public void testFibonacci() {
+        Assertions.assertEquals(0, fib(0));
+    }
+
+    int fib(int n) {
+        return 0;
+    }
 }
