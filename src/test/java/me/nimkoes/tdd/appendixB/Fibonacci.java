a6902367f0ac51dae72a272473d021c8e2b053af
nimkoes <xxxelppa@gmail.com>
2023-08-06 19:27:42 +0900

    부록 B. 피보나치 - 작은 입력을 특별한 경우로 다루겠다.

--- src/test/java/me/nimkoes/tdd/appendixB/Fibonacci.java
+++ src/test/java/me/nimkoes/tdd/appendixB/Fibonacci.java
@@ -8,16 +8,15 @@ public class Fibonacci {
 
     @Test
     public void testFibonacci() {
-        int cases[][] = {{0, 0}, {1, 1}, {2, 1}};
+        int cases[][] = {{0, 0}, {1, 1}, {2, 1}, {3, 2}};
         for (int i = 0; i < cases.length; i++) {
             assertEquals(cases[i][1], fib(cases[i][0]));
         }
     }
 
     int fib(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        return 1;
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+        return 2;
     }
 }
